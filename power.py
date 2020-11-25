fbdc78c6a8be04f6aed4a456ca144bf0844564f3
Dillon Huff <dillonhuff@gmail.com>
2020-11-25 01:14:24 -0600

    Power results actually kind of make sense

--- power.py
+++ power.py
@@ -40,36 +40,48 @@ cp_pes = 397
 cp_mems = 8
 # cp_total = cp_pes + cp_mems
 
-def apply_model(pe_count, mem_count):
+cp_cycle_exe_time = float(4122)
+cp_ops_per_cycle = float(397)
+
+def apply_model(pe_count, mem_count, cycle_exe_time, ops_per_cycle):
+
+    cycles_per_sec = float(1e9)
     cp_total = pe_count + mem_count
     m_pe_power = pe_power_model[0]*cp_pes + pe_power_model[1]
     m_mem_power = mem_power_model[0]*cp_mems + mem_power_model[1]
     m_ic_power = ic_power_model[0]*cp_total + ic_power_model[1]
 
     m_power_mw = m_pe_power + m_mem_power + m_ic_power
-    print('Total CP power in mW:', m_power_mw)
-    return m_power_mw
+    print('Total power in mW:', m_power_mw)
+    # return m_power_mw
 
-cp_power_mw = apply_model(cp_pes, cp_mems)
-cp_cycle_exe_time = float(4122)
-cycles_per_sec = float(1e9)
+    cp_power_mw = m_power_mw
+    # cp_power_mw = apply_model(cp_pes, cp_mems)
 
-cp_ops_per_cycle = float(397)
+    cp_ops = ops_per_cycle*cycle_exe_time
 
-cp_ops = cp_ops_per_cycle*cp_cycle_exe_time
+    print('# ops in CP:', cp_ops)
 
-print('# ops in CP:', cp_ops)
+    cp_exe_time_sec = cycle_exe_time / cycles_per_sec
 
-cp_exe_time_sec = cp_cycle_exe_time / cycles_per_sec
+    print('Exe time cp (sec):', cp_exe_time_sec)
 
-print('Exe time cp (sec):', cp_exe_time_sec)
+    cp_power_watts = cp_power_mw / 1000
+    print('power (Watts)', cp_power_watts)
 
-cp_power_watts = cp_power_mw / 1000
-print('CP power (Watts)', cp_power_watts)
+    cp_energy_joules = cp_power_watts * cp_exe_time_sec
+    print('Energy consumed =', cp_energy_joules)
 
-cp_energy_joules = cp_power_watts * cp_exe_time_sec
-print('CP Energy consumed =', cp_energy_joules)
+    print('Energy / op =', cp_energy_joules / cp_ops)
 
-print('CP Energy / op =', cp_energy_joules / cp_ops)
+print('==== Camera pipeline...')
+apply_model(cp_pes, cp_mems, cp_cycle_exe_time, cp_ops_per_cycle)
 
+print('\n\n')
+print('==== Mobilenet...')
+mn_pes = 114
+mn_mems = 7 
+mn_cycle_exe_time = 1026
+mn_ops_per_cycle = 114
 
+apply_model(mn_pes, mn_mems, mn_cycle_exe_time, mn_ops_per_cycle) 
